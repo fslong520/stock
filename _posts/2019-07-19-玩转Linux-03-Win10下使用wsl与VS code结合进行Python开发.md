a1f753d8102d379035758f6e8ecbd3b3dcea1fe6
fslong <fsl470657570@outlook.com>
2019-07-19 12:08:03 +0800

    update

---
+++ _posts/2019-07-19-玩转Linux-03-Win10下使用wsl与VS code结合进行Python开发.md
@@ -0,0 +1,79 @@
+---
+layout: post
+title: '2019-07-19-玩转Linux-03-Win10下使用wsl与VS code结合进行Python开发'
+date: 2019-7-19
+author: fslong
+cover: ''
+tags: linux,wsl,Python
+---
+  
+&emsp;&emsp;*“不积跬步无以至千里，不积小流无以成江海”，这个系列是我在玩Linux的时候写的一些东西。*  
+   
+
+---
+&emsp;&emsp;先交代下故事背景：现在Win10最新的预览版已经加入了wsl2，已经支持了docker，用了Linux4.19内核，非常的好用，同时性能杠杠的，笔者一台好久之前的笔记本wsl2使用Unixbench跑分大约1620分，deepin跑分1700分，基本上可以理解为误差了。由于工作关系，本人用了一段时间的Deepin，实事求是地讲Deepin非常的不错，日常使用也完全没问题的，但我遇到了在移动文件时候文件丢失的问题（exFat分区→Ntfs分区）然后就凉凉了，并且还是离不开OneDrive还有Microsoft office全家桶，于是叛变了两个礼拜的我又回到了Win10的怀抱。但大家都知道，Windows下面Python开发坑也不少，挺难受的，那有没有二者兼得的方法？答案是有的，那就是使用wsl和VS code结合的方法。但是网上很多教程其实都有点问题，有些不好用，有些配置比较麻烦，本人意外的发现VS code官方文档其实已经有了推荐的方法，本文在此基础上夹带了一些私货，供大家体验。由于很多小伙被没有升级到最新的预览版，用不了wsl2，下文以另外一台只有wsl1的电脑为例讲解如何使用。  
+
+### 一、原理   
+&emsp;&emsp;原理其实很简单，用的是远程连接wsl。是使用VS code远程连接WSL进行开发，但是由于wsl本身就在本地，还可以跟Win10系统的本地文件交互（现在性能已经挺好了），所以实际上在这些文件的操作又可以使用Win10系统，非常的舒服。       
+
+### 二、配置方法 
+#### （一）、第一步自然是安装VScode和wsl，相关教程很多，不在这里赘述，最终结果如下图所示：   
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//debian.jpg">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//vscode.jpg">
+</p>  
+
+#### (二)、在vscode里面安装插件`remote-wsl`，如下图：  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//vscode.jpg">
+</p>   
+
+安装完了之后，我们点击左下角的和`><`按钮，在上方框框里选择`Ropen folder in wsl`，如下图所示：  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//remote-wsl.jpg">
+</p>    
+
+首次运行得等会儿，会安装一些服务，然后有`Node.js`的联网请求，当然是点允许，如下图所示：  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//node.jpg">
+</p>  
+
+之后的界面就会变成如下：
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//completely.jpg">
+</p>  
+
+这里有个需要注意的地方，其实在wsl里面我们也装了一个vscode的服务，我们现在在vscode里面安装插件的时候可以看到左侧有个`local`还有个`wsl`分类，如下图所示：  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//localAndWsl.jpg">
+</p>  
+
+这就表示其实我们编程的时候很多东西都是调用的wsl里头的东西，所以我们安装插件的时候要想在wsl里面也能用，必须要把他安装到wsl里面才能使用，比如下面的语言包：  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//Chinese.jpg">
+</p>  
+
+**其实就是用VScode远程连接进行开发，没什么特殊的！**  
+  
+
+需要注意的是: <font color="red">安装完这个插件以后，当我们在打开文件夹的时候就会有两种提示，一种是打开本地，一种是打开wsl。由于在wsl中电脑磁盘是挂载在`/mnt/`目录下的，所以在打开wsl目录的时候就会有以下的提示：</font>  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//openFolder.jpg">
+</p>  
+
+如果我们点击右边的`show local`自然是打开Windows目录下的文件夹，使用起来很方便，如下图：  
+<p align="center">
+    <img src="https://raw.githubusercontent.com/wiki/fslong520/blog//openLocal.jpg">
+</p>    
+
+不过个人还是建议直接一直就在wsl下得了，反正也能直接读写windows10的目录，笔者直接就是在OneDrive当中开发（日常关着的，过几天启动下OneDrive同步一次），这样不影响性能还是同步，还能使用wsl，简直美滋滋。
+
+
+
+&emsp;&emsp;**到这里教程就结束了，这个教程很简单，就是教大家怎么使用wsl和vscode配合进行开发，具体能产生什么样的火花还需要你来开发！**
+
+
+---   
+  
+> **声明：**
+> 微信公众平台对于markdown支持不够友好，建议阅读原文。
--- dev/pythonAPP/img.py
+++ dev/pythonAPP/img.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os, traceback, sys
+import os, traceback, sys, time
 sys.setrecursionlimit(10 * 10)
 
 
-def createImg():
-    inputs = input('请输入需要生成图片代码的文件夹地址(blog.wiki/img下面的地址):')
+def createImg(inputs=None):
+    if inputs == None:
+        inputs = ''
     dirName = inputs
     path = os.path.abspath(__file__)
     for i in range(4):
         path = os.path.dirname(path)
     dirName = os.path.join(path, 'blog.wiki', 'img', dirName)
-    files = os.listdir(dirName)
+    #files = os.listdir(dirName)# 无法遍历子目录
+    files = []
+    splitString = os.path.join('blog.wiki', 'img', dirName)
+    for parent, dirnames, filenames in os.walk(dirName):
+        for filename in filenames:
+            trueFileName = os.path.join(parent, filename)
+            if len(trueFileName.split(splitString)) > 1:
+                files.append(trueFileName.split(splitString)[1])
     imgs = []
 
     for i in files:
@@ -20,30 +28,58 @@ def createImg():
                 imgs.append(os.path.join('img', inputs, i))
     for i in range(len(imgs)):
         imgs[i] = imgs[i].replace('\\', '/')
-    htmlCode = '<p align="center">\n'
-    markdownCode = '<p align="center">  \n'
+    htmlCode = ''
+    markdownCode = ''
     for i in imgs:
-        htmlCode += '    <img src="https://raw.githubusercontent.com/'
-        htmlCode += 'wiki/fslong520/blog/{}">\n'.format(i)
-        markdownCode += '![](https://raw.githubusercontent.com/'
-        markdownCode += 'wiki/fslong520/blog/{})\n'.format(i)
-    htmlCode += '</p>'
-    markdownCode += '</p>'
-    return '#' * 80 + '\nhtml代码如下：\n{}'.format(
-        htmlCode) + '\n\n\n' + '#' * 80 + '\nmarkdown代码如下：\n{}'.format(
-            markdownCode)
+        htmlCode += '\n<p align="center">\n    <img src="https://raw.githubusercontent.com/'
+        htmlCode += 'wiki/fslong520/blog/{}">\n</p>'.format(i)
+        markdownCode += '\n<p align="center">  \n![](https://raw.githubusercontent.com/'
+        markdownCode += 'wiki/fslong520/blog/{})\n</p>'.format(i)
+    return ('#' * 80 + '\nhtml代码如下：\n{}'.format(htmlCode) + '\n\n\n' +
+            '#' * 80 + '\nmarkdown代码如下：\n{}'.format(markdownCode), len(imgs))
+
+
+def inputData():
+    inputs = input('请输入需要生成图片代码的文件夹地址(blog.wiki/img下面的地址):')
+    try:
+        dirName = inputs
+        path = os.path.abspath(__file__)
+        for i in range(4):
+            path = os.path.dirname(path)
+        dirName = os.path.join(path, 'blog.wiki', 'img', dirName)
+        files = os.listdir(dirName)  # 无法遍历子目录
+    except Exception:
+        print('请输入正确的文件目录！\n')
+        inputData()
+    finally:
+        return inputs
+
+
+def createImgFile(inputs):
+    pypath = os.path.join(os.path.dirname(__file__), 'imgs.html')
+    with open(pypath, 'w+', encoding='utf-8', errors='ignore') as f:
+        data = createImg(inputs)
+        f.write(data[0])
+    return data[1]
 
 
 if __name__ == "__main__":
 
-    def createImgFile():
-        pypath = os.path.join(os.path.dirname(__file__), 'imgs.html')
-        try:
-            with open(pypath, 'w+', encoding='utf-8', errors='ignore') as f:
-                data = createImg()
-                f.write(data)
-        except Exception:
-            traceback.print_exc()
-            createImgFile()
+    inputs = inputData()
+    count = 0
+    if inputs == '':
+        infoString = '图片'
+    else:
+        infoString = inputs
 
-    createImgFile()
+    while True:
+        try:
+            count += 1
+            print('开始第{}次生成{}目录下照片文件图床链接请稍候...'.format(count, infoString))
+            imgNum = createImgFile(inputs)
+            print('{}目录链接生成完毕，总共有{}张图片，请前往./imgs.html文件查看。\n'.format(
+                infoString, imgNum))
+            time.sleep(3)
+        except KeyboardInterrupt:
+            print('谢谢使用')
+            break
